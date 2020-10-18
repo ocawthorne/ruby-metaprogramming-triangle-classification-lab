de2651659381f3d40231c7d33fa6b15f1d01d85f
ocawthorne <ocawthorne@gmail.com>
2020-10-18 21:17:47 +0100

    triangle-101820

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  @@types = [:equilateral, :isosceles, :scalene]
+
+  def initialize(s1, s2, s3)
+    @triangle = [s1, s2, s3]
+  end
+
+  def kind
+    t = @triangle
+    raise TriangleError if t.any?{|s| s >= t.sum - s}
+    @@types[t.uniq.count-1]
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "Not a triangle."
+    end
+  end
+
 end
